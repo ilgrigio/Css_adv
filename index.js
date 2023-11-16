e4c50eb2dc516912bcd4682b9f83cad2f9a17f11
Andrea Sattin <andsattin@gmail.com>
2023-11-16 14:18:41 +0100

    tentativi js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let aLink = 
-const removeLink = function {
+// let aLink = document.querySelector("ol.list-unstyled li:nth-of-type(2)");
+// const removeLink = function () {
+//   aLink.classList.add("displayNone");
+// };
+// removeLink();
+let aLink = document.querySelectorAll("h2");
 
-}
+const removeLink = function () {
+  aLink.innerText = "Ciao pippo";
+};
+console.log(removeLink());
