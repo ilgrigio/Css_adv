1584921ad2f2e9ecbe60f4f21497a9d0fbbe8dab
Andrea Sattin <andsattin@gmail.com>
2023-11-17 01:50:50 +0100

    Extra finiti

--- index.js
+++ index.js
@@ -3,20 +3,34 @@
 //   aLink.classList.add("displayNone");
 // };
 // removeLink();
+// Extra 1)
 const liReset = document.querySelectorAll("li");
 for (let i = 0; i < liReset.length; i++) {
   if (liReset[i].textContent.includes("Twitter")) {
     liReset[i].remove();
   }
 }
+// Extra 2
+
+const selectAuth = document.querySelector(
+  ".col.p-4.d-flex.flex-column.position-static"
+);
+const eraseParent = function () {
+  selectAuth.remove();
+  // const selectContinue = document.querySelector(".stretched-link");
+};
+selectAuth.addEventListener("click", eraseParent);
+
+// Extra 3
 const alertMessage = function () {
   alert("Questo è l'autore dell'articolo");
 };
 const nomeAutore = function () {
   const aSelector = document.querySelectorAll("p.blog-post-meta a");
   for (let i = 0; i < aSelector.length; i++) {
-    console.log(aSelector[i].innerHTML);
+    // console.log(aSelector[i].innerHTML);
     aSelector[i].addEventListener("mouseover", alertMessage);
   }
 };
-console.log(nomeAutore());
+nomeAutore();
+// console.log(nomeAutore());
